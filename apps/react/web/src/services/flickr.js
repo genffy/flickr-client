c62ad0d6d0cfd485e13095bd01e14324c7fac4a6
genffy lee <dpDev@genffy.local>
2016-06-29 11:52:10 +0800

    update package.json

--- apps/react/web/src/services/flickr.js
+++ apps/react/web/src/services/flickr.js
@@ -2,9 +2,7 @@
  * Created by genffy on 16/6/5.
  */
 
-import Flickr from 'flickrapi/browser/test.js'
-
-console.log(Flickr)
+import Flickr from 'flickrapi/browser/flickrapi.js'
 
 const flickr = new Flickr({
    api_key: '90cf3303994db18bd620941cd9d1f131'
